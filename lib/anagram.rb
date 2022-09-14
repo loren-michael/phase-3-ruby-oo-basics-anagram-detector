e27f9f7427671564919221f035545ecb202c3839
loren <lorenmichaelh@gmail.com>
2022-09-14 15:02:17 -0500

    commit tree d2477801cd6ebf110bc011d8a09d439e40e1e598

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+class Anagram
+
+  def initialize(string)
+    @name = string
+  end
+  
+  def match(array)
+    array.select { |x| x.split("").sort == @name.split("").sort }
+  end
+
+
+end
